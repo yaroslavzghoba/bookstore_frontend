1a08b1022bb4387bf7b3528c16e7cf38d7bb7b21
Yaroslav Zghoba <yaroslavzghoba@gmail.com>
2024-12-16 01:06:55 +0100

    Added an entry point for the application

---
+++ composeApp/src/desktopMain/kotlin/org/example/bookstore/main.kt
@@ -0,0 +1,13 @@
+package org.example.bookstore
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Bookstore",
+    ) {
+        App()
+    }
+}
